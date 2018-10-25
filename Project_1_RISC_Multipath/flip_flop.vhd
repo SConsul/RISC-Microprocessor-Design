1b3f875aa9c155bea6aeb794d2875a60744993ab
Parthasarathi Khirwadkar <kparth98@gmail.com>
2018-10-25 23:24:18 +0530

    Adding flip flop without clear

---
+++ Project_1_RISC_Multipath/flip_flop.vhd
@@ -0,0 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity flip_flop is
+  port (D, EN, CLK: in std_logic; Q: out std_logic);
+end entity;
+
+architecture FlipFlop of flip_flop is
+signal temp: std_logic;
+begin
+
+   process(CLK, EN)
+   begin
+
+  if CLK'event and (CLK = '1') then
+    if(EN='1') then
+        Q <= D;
+      end if;
+	 end if;
+   end process;
+
+end FlipFlop;
