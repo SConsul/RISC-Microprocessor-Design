448d65ffb0565851efe887cd81d4e3288f0efc90
SConsul <sarthakconsul@gmail.com>
2018-10-27 19:53:46 +0530

    odified memory

--- Project_1_RISC_Multipath/memory.vhd
+++ Project_1_RISC_Multipath/memory.vhd
@@ -3,22 +3,21 @@ entity memory is
   port(clk: in std_logic;
       mem_write_bar: in std_logic
       address: in std_logic_vector(15 downto 0);
-      data: inout std_logic_vector(15 downto 0));
+      data_in: in std_logic_vector(15 downto 0));
 end entity;
 
 architecture mem of memory is
-  type RAM_array is array (0 to 2**16-1) of std_ulogic_vector (15 downto 0);
+  type RAM_array is array (0 to 2**16-1) of std_logic_vector (15 downto 0);
 
 begin
   process(clk, mem_write_bar, data, address)
     begin
-    if(mem_write_bar = '0') then
-      if rising_edge(clk) then
-        RAM_array(to_integer(unsigned(address)))<= data;
+    if rising_edge(clk) then
+      if(mem_write_bar = '0') then
+        RAM_array(to_integer(unsigned(address)))<= data_in;
       end if;
-    else
-      data <= RAM_array(to_integer(unsigned(address)));
     end if;
+      data_out <= RAM_array(to_integer(unsigned(address)));
   end process;
 end architecture mem;
 T2toR7
