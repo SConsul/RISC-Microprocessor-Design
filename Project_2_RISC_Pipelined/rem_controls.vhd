d05a00678198556162d448131be4473194ee8183
Parthasarathi Khirwadkar <kparth98@gmail.com>
2018-11-30 12:44:26 +0530

    removed hbit

--- Project_2_RISC_Pipelined/rem_controls.vhd
+++ Project_2_RISC_Pipelined/rem_controls.vhd
@@ -128,8 +128,8 @@ begin
 				IF_en<='1';
 			elsif((OR_intermediate_reg(35 downto 32) = "0110") and (OR_intermediate_reg(8) = '0') and (PE1_op /= "00000000")) then
 				PC_en_control <= '0';
-				ID_en<=not(hbit_op);
-				ID_en_8bits<=not(hbit_op);
+				ID_en<='0';
+				ID_en_8bits<='0';
 				PC_control <= pc_control_lmlhi;
 				validate_control_if<='1';
 				nullify_control_id<=not validate_IF;
